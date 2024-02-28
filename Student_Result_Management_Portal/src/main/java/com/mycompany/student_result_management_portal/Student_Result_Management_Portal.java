660010c4f8b63b9a6062c3ee9e2620c12b5d27a2
Srikanth Naik <48707298+Sriku010203@users.noreply.github.com>
2024-02-29 03:35:38 +0530

    Add files via upload

---
+++ Student_Result_Management_Portal/src/main/java/com/mycompany/student_result_management_portal/Student_Result_Management_Portal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.student_result_management_portal;
+
+/**
+ *
+ * @author srika
+ */
+public class Student_Result_Management_Portal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
